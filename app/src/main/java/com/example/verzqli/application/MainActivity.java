08f92e38ddf7a12792eb7c0abf57c7406b03da64
xupei <xupei@aiyaapp.com>
2017-10-25 19:01:05 +0800

    1

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -3,14 +3,13 @@ package com.example.verzqli.application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("zzzz", "222222");
+        Log.i("zzzz", "222222222");
 
 
     }
