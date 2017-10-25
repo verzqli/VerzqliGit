1222f3bbce5e7ce5403d12cde5bc14ffc26ed3b3
xupei <xupei@aiyaapp.com>
2017-10-25 16:47:13 +0800

    xp的提交

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("zzzz", "onCregixte重新开始origin");
         Log.i("zzzz", "onCregixtexpxpzzzzzz22222: ooooooooogit remote –v origin");
 
 
