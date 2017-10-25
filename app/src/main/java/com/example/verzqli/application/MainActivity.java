6c5599c0e732ff0861af1bf3d7dcaf0b9ef1b01b
xupei <xupei@aiyaapp.com>
2017-10-25 16:49:14 +0800

    xp的提交

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("zzzz", "onCregixte重新开始origin");
+        Log.i("zzzz", "onCregixte重新开始给我的分支");
 
 
     }
