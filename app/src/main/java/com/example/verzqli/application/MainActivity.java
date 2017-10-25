fc8960edb9967cc628f2b09168b17e611edc7475
xupei <xupei@aiyaapp.com>
2017-10-25 16:39:22 +0800

    重新开始push

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -10,6 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("zzzz", "onCregixtexpxpzzzzzz22222: ooooooooogit remote –v");
+
+
         Log.i("zzzz", "onCreate:重新开始xp2 22222");
     }
 }
