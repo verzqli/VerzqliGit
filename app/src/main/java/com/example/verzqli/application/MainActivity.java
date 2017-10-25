3e9dd9da4a54c3e78967617f21a188d23d77fb92
xupei <xupei@aiyaapp.com>
2017-10-25 16:43:17 +0800

    xp2的提交

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("zzzz", "onCreate: ");
+        Log.i("zzzz", "onCreate:这是xp2的提交内容 ");
     }
 }
