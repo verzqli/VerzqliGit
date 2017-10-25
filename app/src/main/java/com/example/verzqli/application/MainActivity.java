07090ce2e031028980d4856f91f488a7f0e76dfe
xupei <xupei@aiyaapp.com>
2017-10-25 17:37:05 +0800

    合并完成

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -10,9 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("zzzz", "onCregixte重新开始");
+        Log.i("zzzz", "onCregixte重新开始徐沛22222222");
 
-
-        Log.i("zzzz", "onCreate:重新开始xp2 22222");
     }
 }
