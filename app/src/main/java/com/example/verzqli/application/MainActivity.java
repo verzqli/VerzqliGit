b4cd019be475a50558a77da842c2e154b329d942
xupei <xupei@aiyaapp.com>
2017-12-07 16:17:21 +0800

    切换到以前 代码回来

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private SensorManager sensorManager = null;
