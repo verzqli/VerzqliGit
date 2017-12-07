0c9eb68754d0147c36e2ff77f4f2cd2a4188edfd
xupei <xupei@aiyaapp.com>
2017-12-07 18:38:09 +0800

    新的提交

--- app/src/main/java/com/example/verzqli/application/MainActivity.java
+++ app/src/main/java/com/example/verzqli/application/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private SensorManager sensorManager = null;
@@ -19,11 +20,17 @@ public class MainActivity extends AppCompatActivity {
                 .getDefaultSensor(Sensor.TYPE_ORIENTATION);
         Log.i(TAG, "onCreate: 重新commit11111111    commit3添加顺便修改了111111");
 
-        Log.i(TAG, "onCreate: 重新commit222222222");
+        Log.i(TAG, "顺便修改了33333333");
 
-        Log.i(TAG, "onCreate: 重新commit222222222 回退版本顺便修改");
+        Log.i(TAG, "我还顺便修改了2222222给你加33333");
 
 
         Log.i(TAG, "onCreate: 第二次回退版本 然后加了东西");
+
+        Log.i(TAG, "onCreate: 111111111");
+
+        Log.i(TAG, "onCreate: 222222222");
+
+        Log.i(TAG, "onCreate: 333333333");
     }
 }
